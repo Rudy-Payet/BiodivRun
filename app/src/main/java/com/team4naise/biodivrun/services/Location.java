b7f66d9bba873d47dce45097ff79528db3804912
aliosl <benjamintecher5@gmail.com>
2026-05-19 13:44:56 +0400

    Amélioration location avec lien vers mainActivity

--- app/src/main/java/com/team4naise/biodivrun/services/Location.java
+++ app/src/main/java/com/team4naise/biodivrun/services/Location.java
@@ -10,15 +10,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.location.*;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.team4naise.biodivrun.data.Zone;
 
 public class Location {
+
+
+    public interface OnZoneCalculeeListener {
+        void onZoneCalculee(double latMin, double latMax, double lonMin, double lonMax);
+    }
 
     private final AppCompatActivity activity;
     private FusedLocationProviderClient fusedLocationClient;
     private LocationCallback locationCallback;
     private int rayonKm = 10;
+
+
+    private OnZoneCalculeeListener listener;
+    private android.location.Location dernierePosition;
 
     private final ActivityResultLauncher<String> permissionLauncher;
 
@@ -40,6 +47,7 @@ public class Location {
                 if (result == null) return;
                 android.location.Location loc = result.getLastLocation();
                 if (loc != null) {
+                    dernierePosition = loc; // sauvegarder position
                     calculerZone(loc);
                 }
             }
@@ -69,12 +77,27 @@ public class Location {
         double degLat = (double) rayonKm / 111.0;
         double degLon = (double) rayonKm / (111.0 * Math.cos(Math.toRadians(centre.getLatitude())));
 
-        double latMin = centre.getLatitude() - degLat;
-        double latMax = centre.getLatitude() + degLat;
+        double latMin = centre.getLatitude()  - degLat;
+        double latMax = centre.getLatitude()  + degLat;
         double lonMin = centre.getLongitude() - degLon;
         double lonMax = centre.getLongitude() + degLon;
 
-        // Bounding box prête à être utilisée
+        //
+        if (listener != null) {
+            listener.onZoneCalculee(latMin, latMax, lonMin, lonMax);
+        }
+    }
+
+    // setters
+    public void setOnZoneCalculeeListener(OnZoneCalculeeListener listener) {
+        this.listener = listener;
+    }
+
+    public void setRayonKm(int rayonKm) {
+        this.rayonKm = rayonKm;
+        if (dernierePosition != null) {
+            calculerZone(dernierePosition);
+        }
     }
 
     public void stop() {
